ce3b7e0170af7e543292a5c7429709478a130650
NikolayCheremnov <coolnick1999@gmail.com>
2024-12-21 17:47:54 +0700

    Добавлен GreetingService

---
+++ GitPenProbeExample/GreetingService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPenProbeExample
+{
+    public class GreetingService
+    {
+        private Dictionary<string, string> greetings;
+
+        public GreetingService()
+        {
+            greetings = new Dictionary<string, string>
+        {
+            { "en", "Hello" },
+            { "ru", "Привет" },
+            { "fr", "Bonjour" },
+            { "es", "Hola" },
+            { "de", "Hallo" },
+            { "ko", "안녕하세요" }
+        };
+        }
+
+        public string GetGreeting(string locale)
+        {
+            if (greetings.TryGetValue(locale, out string? greeting))
+            {
+                return greeting;
+            }
+            else
+            {
+                return "Извините, я не знаю такую локаль.";
+            }
+        }
+    }
+}
