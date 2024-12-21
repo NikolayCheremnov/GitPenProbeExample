2dfedbb0b951621a9593dc29d9d42fc08eff0642
NikolayCheremnov <coolnick1999@gmail.com>
2024-12-21 17:48:11 +0700

    Добавлено использование GreetingService

--- GitPenProbeExample/Program.cs
+++ GitPenProbeExample/Program.cs
@@ -1,36 +1,5 @@
-﻿// Словарь для хранения приветствий на разных языках
-var greetings = new Dictionary<string, string>
-{
-    { "en", "Hello" },
-    { "ru", "Привет" },
-    { "fr", "Bonjour" },
-    { "es", "Hola" },
-    { "de", "Hallo" },
-    { "ko", "안녕하세요" }
-};
+﻿using GitPenProbeExample;
 
-string? input;
-do
-{
-    Console.Write("Введите код локали (например, en, ru) или 'exit' для выхода: ");
-    string locale = Console.ReadLine()?.ToLower() ?? string.Empty;
-
-    if (locale == "exit")
-    {
-        break;
-    }
-
-    if (greetings.TryGetValue(locale, out string? greeting))
-    {
-        Console.WriteLine($"{greeting}!");
-    }
-    else
-    {
-        Console.WriteLine("Извините, я не знаю такую локаль.");
-    }
-
-    Console.Write("Хотите попробовать еще раз? (да/нет): ");
-    input = Console.ReadLine()?.ToLower();
-} while (input == "да");
-
-Console.WriteLine("Программа завершена.");
+GreetingService greetingService = new GreetingService();
+UserInterface userInterface = new UserInterface(greetingService);
+userInterface.Run();
---
+++ GitPenProbeExample/UserInterface.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPenProbeExample
+{
+    public class UserInterface
+    {
+        private GreetingService greetingService;
+
+        public UserInterface(GreetingService greetingService)
+        {
+            this.greetingService = greetingService;
+        }
+
+        public void Run()
+        {
+            string? input;
+            do
+            {
+                Console.Write("Введите код локали (например, en, ru) или 'exit' для выхода: ");
+                string locale = Console.ReadLine()?.ToLower() ?? string.Empty;
+
+                if (locale == "exit")
+                {
+                    break;
+                }
+
+                string greeting = greetingService.GetGreeting(locale);
+                Console.WriteLine($"{greeting}!");
+
+                Console.Write("Хотите попробовать еще раз? (да/нет): ");
+                input = Console.ReadLine()?.ToLower();
+            } while (input == "да");
+
+            Console.WriteLine("Программа завершена.");
+        }
+    }
+}
